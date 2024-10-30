3d680403ed0f83674f717cf22905fa1d405eab83
SirMrCC <ccubed.cx3@gmail.com>
2024-10-29 20:11:14 -0400

    initial-commit-2

---
+++ src/main/java/sirmrcc/alchemy/CCsModAlchemy.java
@@ -0,0 +1,18 @@
+package sirmrcc.alchemy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CCsModAlchemy implements ModInitializer
+{
+	public static final String MOD_ID = "cc-mod-alchemy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
---
+++ src/main/java/sirmrcc/alchemy/CCsModAlchemyClient.java
@@ -0,0 +1,12 @@
+package sirmrcc.alchemy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CCsModAlchemyClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
